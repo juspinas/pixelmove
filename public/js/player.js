0b8280491479aaf614dddb57d0de345102648c44
juspinas <justin.espinas@gmail.com>
2021-09-27 00:11:54 +1300

    Added all platforms to jump on

--- public/js/player.js
+++ public/js/player.js
@@ -9,7 +9,7 @@ class player {
         this.gravity = 5;
         this.yspeed = 0;
         this.maxspeed = 10;
-        this.xspeed = 0;
+        this.xspeed = 0; //Current speed
 
         objects.push(this);
     }
@@ -19,9 +19,13 @@ class player {
         // this.x = mouseX;
         // this.y = mouseY;
         this.yspeed += this.gravity;
-        var ground = wallCollision(this.bbx,this.x,this.y+1);
-        if(keyPress.W && ground) this.yspeed = -20;
 
+        // Jumping
+        var jumpheight = 35;
+        var ground = wallCollision(this.bbx,this.x,this.y+1);
+        if(keyPress.W && ground) this.yspeed = - jumpheight;
+
+        // Vertical collision
         if (wallCollision(this.bbx, this.x, this.y+this.yspeed)) {
             while(!wallCollision(this.bbx, this.x, this.y + Math.sign(this.yspeed))) {
                 this.y += Math.sign(this.yspeed)
@@ -31,9 +35,11 @@ class player {
         this.y += this.yspeed;
         this.bbx.update(this.x, this.y);
 
+        // Horizontal movement
         var d = keyDown.D - keyDown.A;
         this.xspeed = d * this.maxspeed;
 
+        // Horizontal collision
         if (wallCollision(this.bbx, this.x + this.xspeed, this.y)) {
             while(!wallCollision(this.bbx, this.x + Math.sign(this.xspeed), this.y)) {
                 this.x += Math.sign(this.xspeed)
